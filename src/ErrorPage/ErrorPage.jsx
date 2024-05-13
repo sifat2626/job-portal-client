513a680767a9850d675c3e32234aa01c78142b99
sifat2626 <ashrafulsifat26@gmail.com>
2024-05-13 11:23:00 +0600

    404 added

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
+import img from "../assets/images/404.jpg";
 
 function ErrorPage() {
   return (
     <section className="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-400">
-            <span className="sr-only">Error</span>404
-          </h2>
+          <img src={img} alt="" />
           <p className="text-2xl font-semibold md:text-3xl">
             Sorry, we couldn&apos;t find this page.
           </p>
@@ -17,7 +16,7 @@ function ErrorPage() {
           <Link
             to="/"
             rel="noopener noreferrer"
-            className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+            className="px-8 py-3 font-semibold rounded  bg-job dark:text-gray-50"
           >
             Back to homepage
           </Link>
