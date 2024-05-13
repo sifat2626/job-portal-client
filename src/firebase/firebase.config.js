c6015fc6009a73d936bd471df11f6a01f308a55d
sifat2626 <ashrafulsifat26@gmail.com>
2024-05-13 11:49:36 +0600

    firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAK0YR-FE18QvYFwggxIFTB7FraN_gDecQ",
-  authDomain: "webby-522d9.firebaseapp.com",
-  projectId: "webby-522d9",
-  storageBucket: "webby-522d9.appspot.com",
-  messagingSenderId: "684022046131",
-  appId: "1:684022046131:web:1b2639f215d6fe16e63fc6",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
